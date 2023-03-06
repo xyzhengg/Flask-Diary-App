ba3d04dc61283f0d72ce298eb39b1a1db13947d5
Xin Yu Zhenge <xy.zheng@live.com>
2023-03-06 17:14:00 +1100

    still on eiditng/deleting. month dates still not working.

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 let monthSelection = document.querySelector('.select-month')
 let yearSelection = document.querySelector('.select-year')
 let dateSelection = document.querySelector('.select-date')
+let dateList = document.querySelectorAll('.date-list')
 
 monthSelection.addEventListener("change", updateDateOptions)
 yearSelection.addEventListener("change", updateDateOptions)
@@ -12,19 +13,20 @@ function updateDateOptions() {
     let selectedDate = parseInt(dateSelection.value)
     console.log(selectedDate)
     const leapYear = selectedYear % 4 === 0 && (selectedYear % 100 !== 0 || selectedYear % 400 === 0)
-    if (selectedMonth === 2 && selectedYear == leapYear) {
-        for (selectedDate > 29) {
-            dateSelection.style.display = "none";
+    console.log(leapYear)
+    if (selectedMonth == 2 && leapYear) {
+        if (dateList.value > 29) {
+            dateList.style.display="none";
         }
-    } else if (selectedMonth === 2 && !leapYear) {
-        if (selectedDate > 28) {
-            dateSelection.style.display="none"
+    } else if (selectedMonth == 2 && !leapYear) {
+        if (dateList.value > 28) {
+            dateList.style.display="none";
         }
-    } else if (selectedMonth === 4 || selectedMonth === 6 || selectedMonth === 9 || selectedMonth === 11){
-        if (selectedDate > 30) {
-            dateSelection.style.display="none"
+    } else if (selectedMonth == 4 || selectedMonth == 6 || selectedMonth == 9 || selectedMonth == 11){
+        if (dateList.value > 30) {
+            dateList.style.display="none";
         }
     } else {
-        dateSelection.style.display="block"
+        dateList.style.display="block";
     }
 }
